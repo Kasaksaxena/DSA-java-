89fd0f0749dc0de3b17d9dbb4cc198c78fb54894
Kasaksaxena <kasaksaxena6@gmail.com>
2026-07-16 19:04:04 +0530

    lc 392

---
+++ Strings/LC387.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class LC387 {
+        public static int firstUniqChar(String s) {
+
+            int[] freq = new int[26];
+
+            for(char c : s.toCharArray()){
+                freq[c -'a']++;
+            }
+
+            for(int i =0; i < s.length(); i++){
+                if(freq[s.charAt(i) -'a'] == 1){
+                    return i;
+                }
+            }
+        return -1;
+        }
+            public static void main(String[] args) {
+
+        String s = "loveleetcode";
+
+        System.out.println(firstUniqChar(s));
+    }
+
+    
+}
