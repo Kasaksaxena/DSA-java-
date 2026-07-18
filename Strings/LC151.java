d9a5e1f5b8eb9fde0e96a0fce4710d86d252f6d6
Kasaksaxena <kasaksaxena6@gmail.com>
2026-07-18 19:59:00 +0530

    lc 151

---
+++ Strings/LC151.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class LC151 {
+
+    public static String reverseWords(String s) {
+
+        String[] words = s.trim().split("\\s+");
+
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+
+            ans.append(words[i]);
+
+            if (i != 0) {
+                ans.append(" ");
+            }
+        }
+
+        return ans.toString();
+    }
+
+    public static void main(String[] args) {
+
+        String s = "  the sky   is blue  ";
+
+        System.out.println(reverseWords(s));
+    }
+}
